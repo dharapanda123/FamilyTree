b3c711f5dd1bb7566c9fe9ea5d7a28350a4e0558
dharapanda123 <dhara.dhara.009@gmail.com>
2015-05-08 22:35:18 +1200

    addedsln for project

---
+++ FamilyTree/FamilyTree/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FamilyTree.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
